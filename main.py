0f8d11945eb4ece1c1d9db5cc7559e6232587856
João Afonso Katombela <ja3328173@gmail.com>
2024-05-12 18:04:33 +0100

    hjjg

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: str, q: Union[str, None] = None):
+def read_item(item_id: str):
     return {"item_id": item_id, "q": q}
 
 
 
 
 @app.get("/get_fb_posts/{query}")
-def read_item(query: str, q: Union[str, None] = None):
+def read_item(query: str):
     posts = []
     for post in get_posts(query, pages=1):
         posts.append(post['text'][:50])
